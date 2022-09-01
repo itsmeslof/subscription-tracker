3c04d7c45ef3558d04063b93f70de976f4c5596e
Slof <slof@hey.com>
2022-09-01 07:26:33 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Brick\Math\RoundingMode;
+use Brick\Money\Context\DefaultContext;
+use Brick\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,7 +46,49 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setAsAdmin() {
+    public function setAsAdmin()
+    {
         $this->update(['is_admin' => true]);
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
+
+    public function getMinorCostOfActiveMonthlySubscriptions()
+    {
+        return $this->subscriptions()->active()->monthly()->pluck('amount')->sum();
+    }
+
+    public function getMinorCostOfAllActiveSubscriptionsAnnually()
+    {
+        return (
+            ($this->getMinorCostOfActiveMonthlySubscriptions() * 12) + 
+            ($this->subscriptions()->active()->semiannually()->pluck('amount')->sum() * 2) + 
+            $this->subscriptions()->active()->annually()->pluck('amount')->sum()
+        );
+    }
+
+    public function getFormattedMonthlySubscriptionCost()
+    {
+        $money = $this->createMoneyFromMinor($this->getMinorCostOfActiveMonthlySubscriptions());
+        return $money->formatTo('en_US');
+    }
+
+    public function getFormattedAnnualSubscriptionCost()
+    {
+        $money = $this->createMoneyFromMinor($this->getMinorCostOfAllActiveSubscriptionsAnnually());
+        return $money->formatTo('en_US');
+    }
+
+    private function createMoneyFromMinor($minorAmount, $currency = 'USD', $context = null, $roundingMode = RoundingMode::DOWN)
+    {
+        return Money::ofMinor(
+            $minorAmount,
+            $currency,
+            $context,
+            $roundingMode
+        );
     }
 }
