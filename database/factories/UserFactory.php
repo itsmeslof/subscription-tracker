e5a49fd335690a851575c6558ea4bcd7c98c9e5b
Slof <slof@hey.com>
2022-09-05 12:22:52 -0500

    Replace 'name' with 'username' in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->safeEmail(),
+            'is_admin' => false,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -35,6 +36,13 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function asAdmin()
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true
         ]);
     }
 }
