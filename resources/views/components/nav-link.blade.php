600810023dcb6df68fb74ca936e7e0e8f227327c
Slof <slof@hey.com>
2022-09-15 01:33:16 -0500

    Update nav-link.blade.php

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,9 @@
 @props(['active'])
-
+{{-- inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out --}}
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'bg-slate-600 rounded-lg inline-flex items-center px-5 py-2 text-sm font-bold leading-5 text-white focus:outline-none focus:ring ring-blue-300 transition ease-in-out duration-150'
+            : 'rounded-lg inline-flex items-center px-5 py-2 text-sm font-medium leading-5 text-slate-500 hover:text-slate-700 hover:bg-slate-600 hover:text-white focus:outline-none focus:ring ring-blue-300 focus:text-white focus:bg-slate-600 transition ease-in-out duration-150';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
