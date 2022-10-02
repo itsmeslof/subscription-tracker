62be1a469b8be0ab6ae0bd4b9d321bdcb51f73b6
Slof <slof@hey.com>
2022-10-02 15:32:24 -0500

    Update nav-link.blade.php

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,8 @@
 @props(['active'])
-{{-- inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out --}}
 @php
 $classes = ($active ?? false)
             ? 'bg-slate-600 rounded-lg inline-flex items-center px-5 py-2 text-sm font-bold leading-5 text-white focus:outline-none focus:ring ring-blue-300 transition ease-in-out duration-150'
-            : 'rounded-lg inline-flex items-center px-5 py-2 text-sm font-medium leading-5 text-slate-500 hover:text-slate-700 hover:bg-slate-600 hover:text-white focus:outline-none focus:ring ring-blue-300 focus:text-white focus:bg-slate-600 transition ease-in-out duration-150';
+            : 'rounded-lg inline-flex items-center px-5 py-2 text-sm font-medium leading-5 text-slate-500 hover:text-slate-100 hover:bg-slate-600 hover:text-white focus:outline-none focus:ring ring-blue-300 focus:text-white focus:bg-slate-600 transition ease-in-out duration-150';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
