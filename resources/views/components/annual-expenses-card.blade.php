8def1a64c065c6a5f881e7a49e41ec8572e78170
Slof <slof@hey.com>
2022-10-19 07:02:56 -0500

    Fix typo

--- resources/views/components/annual-expenses-card.blade.php
+++ resources/views/components/annual-expenses-card.blade.php
@@ -2,7 +2,7 @@
 
 <div class="bg-white border border-slate-300 py-6 px-6 flex flex-col justify-between rounded-lg dark:bg-slate-800 dark:border-slate-600">
     <div class="mb-8">
-        <h4 class="text-slate-600 text-sm mb-2 dark:text-slate-300">Annualy</h4>
+        <h4 class="text-slate-600 text-sm mb-2 dark:text-slate-300">Annually</h4>
         <h2 class="text-slate-700 text-2xl font-bold dark:text-slate-100">{{ $amount }}</h2>
     </div>
 
